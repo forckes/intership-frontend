b6f97864d22277a25b9fc9c2aedc3ae2377316fb
forckes <forckes@proton.me>
2023-10-21 15:59:51 +0300

    add ErrorView

---
+++ src/components/ErrorView/ErrorView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ErrorView: React.FC = () => {
+	return (
+		<div>
+			<h1>Error!</h1>
+			<br />
+			<h1>Sorry, pizzas is unavailable now</h1>
+			<br />
+		</div>
+	);
+};
