56bd8b05394ac39bc0c6f223b47af7487cd00ca0
forckes <forckes@proton.me>
2023-10-21 17:35:16 +0300

    update styles

--- src/pages/Home/Home.styled.tsx
+++ src/pages/Home/Home.styled.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-	height: calc(100vh - 82px);
+	height: calc(100vh - 72px);
 	display: flex;
 	justify-content: center;
 	align-items: center;
