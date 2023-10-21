d4f11dd68afbe342351058c1b3f883279e3584af
forckes <forckes@proton.me>
2023-10-21 21:57:52 +0300

    fix auth bug

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,43 +1,43 @@
 import {
 	IAuthResponse,
-	IRegister,
-	ILogin
+	ILogin,
+	IRegister
 } from '../../store/user/user.interface'
 import Cookies from 'js-cookie'
 import { saveToStorage } from './auth.helper'
-import { axiosClassic } from '../../api/api.interceptor'
+import { axiosClassic, instance } from '../../api/api.interceptor'
 import { REFRESH_TOKEN } from '../../constants/token.constants'
 
 export const AuthService = {
 	async main(type: 'login' | 'register', data: ILogin | IRegister) {
-		// try {
-		const response = await axiosClassic<IAuthResponse>({
-			url: `/${type}`,
-			method: 'POST',
-			data
-		})
-		if (response.data.accessToken) {
-			saveToStorage(response.data)
+		try {
+			const response = await axiosClassic<IAuthResponse>({
+				url: `/auth/${type}`,
+				method: 'POST',
+				data
+			})
+			if (response.data.accessToken) {
+				saveToStorage(response.data)
+			}
+			return response.data
+		} catch (error: any) {
+			throw new Error(error)
 		}
-		return response.data
-		// } catch (error: any) {
-		// 	throw new Error(error)
-		// }
 	},
 	async getNewTokens() {
-		// try {
-		const refreshToken = Cookies.get(REFRESH_TOKEN)
-		const response = await axiosClassic.post<IAuthResponse>(
-			'/login/access-token',
-			{ refreshToken }
-		)
-		if (response.data.accessToken) {
-			saveToStorage(response.data)
+		try {
+			const refreshToken = Cookies.get(REFRESH_TOKEN)
+			const response = await axiosClassic.post<IAuthResponse>(
+				'/auth/login/access-token',
+				{ refreshToken }
+			)
+			if (response.data.accessToken) {
+				saveToStorage(response.data)
+			}
+			return response
+		} catch (error: any) {
+			throw new Error(error)
 		}
-		return response
-		// } catch (error: any) {
-		// 	throw new Error(error)
-		// }
 	}
 }
 
