eccb15349a612f72a92096351251a2e530f31222
forckes <forckes@proton.me>
2023-10-21 20:23:51 +0300

    add ProtectedRoutes

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { FC, useEffect } from 'react'
+import { useAuth } from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
+
+interface Props {
+	children: JSX.Element
+}
+
+const ProtectedRoute: FC<Props> = ({ children }) => {
+	const navigate = useNavigate()
+	const { user } = useAuth()
+
+	useEffect(() => {
+		if (!user) {
+			navigate('/login', { replace: true })
+		}
+	}, [user, navigate])
+
+	return <>{user ? children : null}</>
+}
+
+export default ProtectedRoute
