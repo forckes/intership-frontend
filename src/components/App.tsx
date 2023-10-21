d43eded564cd7519bbb19b31805fa299d1fd09f2
forckes <forckes@proton.me>
2023-10-21 15:55:35 +0300

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react'
+import { Box } from './Box/Box'
+
+export const App: FC = () => {
+	return <Box>React homework template</Box>
+}
---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './components/App'
+import './index.css'
+import { Oval } from 'react-loader-spinner'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+	<React.StrictMode>
+		<Suspense
+			fallback={
+				<Oval
+					height={180}
+					width={180}
+					color='#ff4d00'
+					wrapperStyle={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						height: '700px'
+					}}
+					wrapperClass=''
+					visible={true}
+					ariaLabel='oval-loading'
+					secondaryColor='#f57f0996'
+					strokeWidth={3}
+					strokeWidthSecondary={3}
+				/>
+			}
+		>
+			{/* <Provider store={store}> */}
+			<App />
+			{/* </Provider> */}
+		</Suspense>
+	</React.StrictMode>
+)
