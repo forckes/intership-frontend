59313b24c4424bf2ebdd3dcf57101337fd05c97f
forckes <forckes@proton.me>
2023-10-22 20:41:48 +0300

    add responsible styles

--- src/pages/Profile/Profile.styled.tsx
+++ src/pages/Profile/Profile.styled.tsx
@@ -9,10 +9,17 @@ export const ProfileContainer = styled.div`
 	margin-top: 70px;
 	padding: 10px;
 	border-radius: 24px;
+	@media (max-width: 500px) {
+		margin-top: 20px;
+	}
 `
 
 export const Avatar = styled.img`
 	border-radius: 100%;
+	@media (max-width: 500px) {
+		width: 250px;
+		height: 250px;
+	}
 `
 
 export const AccountInfo = styled.div`
@@ -26,20 +33,32 @@ export const Name = styled.div`
 	font-size: 32px;
 	margin-top: 10px;
 	font-weight: 500;
+	@media (max-width: 500px) {
+		font-size: 28px;
+	}
 `
 
 export const Title = styled.span`
 	font-size: 28px;
 	font-weight: 700;
 	margin-top: 20px;
+	@media (max-width: 500px) {
+		font-size: 24px;
+	}
 `
 
 export const Info = styled.div`
 	font-size: 28px;
 	margin-top: 15px;
 	font-weight: 700;
+	@media (max-width: 500px) {
+		font-size: 24px;
+	}
 `
 export const InfoLabel = styled.span`
 	font-size: 28px;
 	font-weight: 400;
+	@media (max-width: 500px) {
+		font-size: 24px;
+	}
 `
